847e08c677acc7ef6b158cf49c9377f9bb3f9cc8
xMattC <mkcorcoran2015@gmail.com>
2025-12-12 12:18:07 -0500

    checkpoint4

---
+++ test_emotion_detection.py
@@ -0,0 +1,28 @@
+from EmotionDetection.emotion_detection import emotion_detector
+import unittest
+
+class TestEmotionDetector(unittest.TestCase):
+
+    def test_emotion_joy(self):
+        result = emotion_detector('I am glad this happened')
+        self.assertEqual(result['dominant_emotion'], 'joy')
+        
+    def test_emotion_anger(self):
+        result = emotion_detector('I am really mad about this')
+        self.assertEqual(result['dominant_emotion'], 'anger')
+        
+    def test_emotion_disgust(self):
+        result = emotion_detector('I feel disgusted just hearing about this')
+        self.assertEqual(result['dominant_emotion'], 'disgust')
+
+    def test_emotion_sadness(self):
+        result = emotion_detector('I am so sad about this')
+        self.assertEqual(result['dominant_emotion'], 'sadness')
+
+    def test_emotion_fear(self):
+        result = emotion_detector('I am really afraid that this will happen')
+        self.assertEqual(result['dominant_emotion'], 'fear')
+
+if __name__ == '__main__':
+    unittest.main()
+
