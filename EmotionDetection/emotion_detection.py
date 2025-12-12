ecc1f86512aa2ca35934f607ea1442ee0563c2de
xMattC <mkcorcoran2015@gmail.com>
2025-12-12 13:12:42 -0500

    fianl checkpoint

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,27 +1,33 @@
 import requests
 import json
 
-
 def emotion_detector(text_to_analyse):
     """ Emotion predict function using the Watson NLP Library
     """
 
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
-
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-
     input_text = {"raw_document": {"text": text_to_analyse}}
 
     response = requests.post(url, json=input_text, headers=header)
 
-    formatted_response = json.loads(response.text)
+    if response.status_code == 200:
+        formatted_response = json.loads(response.text)
+        emotions = formatted_response['emotionPredictions'][0]['emotion']
+        emotions['dominant_emotion'] = max(emotions, key=emotions.get)
+        
+        return emotions
 
-    emotions = formatted_response['emotionPredictions'][0]['emotion']
-
-    emotions['dominant_emotion'] = max(emotions, key=emotions.get)
-
-    return emotions
-
-
+    elif response.status_code == 400:
+        emotions = {'anger': None, 
+                    'disgust': None, 
+                    'fear': None, 
+                    'joy': None, 
+                    'sadness': None, 
+                    'dominant_emotion': None
+                }
+        
+        return emotions
+    
 if __name__ == "__main__":
-    emotion_detector("“I hate working long hours.”")
+    emotion_detector("")
--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+"""Flask application for emotion detection.
+"""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,16 +7,24 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def sent_analyzer():
-
+    """ Analyse the provided text and return the detected emotions as JSON.
+    """
     text_to_analyze = request.args.get('textToAnalyze')
     result = emotion_detector(text_to_analyze)
+
+    if result['dominant_emotion'] is None:
+        message = "Invalid text! Please try again!"
+        return message
 
     return result
 
 @app.route("/")
 def render_index_page():
+    """ Render the main UI page.
+    """
     return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
 
